c030172c47119f2206e21c75d53c1d0e85f5efc3
Muhammad Afrasyab <afrasyab.cortechsols@gmail.com>
2024-06-24 11:16:24 +0500

    Update the UI of 404

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NotFound = () => {
   return (
-    <div className='d-flex justify-content-center align-items-center h-100'>
-      <h1>404 - Not Found</h1><br></br>
-      <p>The page you are looking for does not exist.</p>
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <h1>404 - Not Found</h1>
     </div>
   );
 }
 
 export default NotFound;
+
